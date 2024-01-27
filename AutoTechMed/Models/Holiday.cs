ae31eb1ea0e960be08dc5c38bfc90cf42e21dc3b
Hihiz <public@mail.ru>
2024-01-27 16:01:47 +0300

    Создать модели

---
+++ AutoTechMed/Models/Holiday.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoTechMed.Models
+{
+    public class Holiday
+    {
+        public DateTime Date { get; set; }
+        public string LocalName { get; set; }
+        public string Name { get; set; }
+        public bool Fixed { get; set; }
+        public bool Global { get; set; }
+
+        public List<string> Types { get; set; }
+    }
+}
---
+++ AutoTechMed/Models/SelectedModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoTechMed.Models
+{
+    public class SelectedModel
+    {
+        public int SelectedYear { get; set; }
+        public string SelectedCountry { get; set; }
+
+        public List<string> Countries { get; set; }
+    }
+}
